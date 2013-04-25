bc23e58fe3e2fe994ce990aa63d586beca8ed5c7
Bcato <macster@Brandon-Catos-MacBook-Pro-15.local>
2013-04-25 12:20:35 -0700

    turn off search func for heroku - need to add websolar to get to work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ ClistApp::Application.configure do
   # In production, :host should be set to the actual host of your application.
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
-  # Paperclip should use Amazon S3 on Heroku 
+  # Config Amazon S3 for image upload with paperclip in Heroku production 
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
